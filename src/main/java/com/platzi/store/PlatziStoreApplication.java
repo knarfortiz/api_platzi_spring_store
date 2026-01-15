5c80daf9ceb8907cc07a50d6d1e946ed82207f36
knarfortiz <o.g.knarf@hotmail.com>
2026-01-14 19:05:16 -0500

    initialize platzi-store project with Spring Boot and basic controller

---
+++ src/main/java/com/platzi/store/PlatziStoreApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/store/PlatziStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
