435dd04e035791b801c11f0df9958f495213c954
knarfortiz <o.g.knarf@hotmail.com>
2026-01-15 12:01:44 -0500

    add database configuration and entity classes for store application

---
+++ src/main/java/com/platzi/store/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,31 @@
+package com.platzi.store.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+    @Column(name = "id_compra")
+    private Integer IdCompra;
+
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    public Integer getIdCompra() {
+        return IdCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        IdCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
